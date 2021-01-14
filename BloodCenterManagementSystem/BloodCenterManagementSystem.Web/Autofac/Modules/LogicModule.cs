81a18f07bf057096e9844f0d6596f3d06abde77e
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:44:36 +0100

    Created Autofac Modules for the purpose of registration of interfaces and their implementations

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/Autofac/Modules/LogicModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using BloodCenterManagementSystem.Logics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.Autofac.Modules
+{
+    public class LogicModule:Module
+    {
+        protected override void Load (ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(ILogic).Assembly)
+                .Where(t => typeof(ILogic).IsAssignableFrom(t))
+                .AsImplementedInterfaces();
+        }
+    }
+}
