82a263ec265cf12bab4eec804c9d2eee02f59fce
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:06:04 +0100

    Created generic repository with Add and SaveChanges method

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/Repository.cs
@@ -0,0 +1,31 @@
+﻿using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class Repository<T>:IRepository<T> where T:BaseModel
+    {
+        private readonly Lazy<DataContext> _dataContext;
+        protected DataContext DataContext => _dataContext.Value;
+
+
+        public Repository(Lazy<DataContext> dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public virtual void Add(T model)
+        {
+            DataContext.Set<T>().Add(model);
+        }
+
+        public virtual void SaveChanges()
+        {
+            DataContext.SaveChanges();
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Repositories
+{
+    public interface IRepository<T> where T:BaseModel
+    {
+        void Add(T model);
+        void SaveChanges();
+    }
+}
