fb59b9812413edd715e1b7e32998ff43e96af70c
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:16:17 +0100

    Created DataContext, Added DbSets, applied mappings added entityframework exceptions handler

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using BloodCenterManagementSystem.DataAccess.Mappings;
+using BloodCenterManagementSystem.Models;
+using EntityFramework.Exceptions.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<BloodDonatorModel> BloodDonators { get; set; }
+        public DbSet<BloodTypeModel> BloodTypes { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserMapping());
+            builder.ApplyConfiguration(new BloodDonatorMapping());
+            builder.ApplyConfiguration(new BloodTypeMapping());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseExceptionProcessor();
+        }
+    }
+}
