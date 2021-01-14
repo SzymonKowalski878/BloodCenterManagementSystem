9f0337d6c2d26a7f9e79596ff2b436d36b8171af
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:09:28 +0100

    Created UserRepository and GetByEmail method

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class UserRepository: Repository<UserModel>,IUserRepository
+    {
+        public UserRepository(Lazy<DataContext> dataContext)
+            :base(dataContext)
+        {
+            
+        }
+
+        public UserModel GetByEmail(string email)
+        {
+            return DataContext.Set<UserModel>().FirstOrDefault(m => m.Email == email);
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Repositories
+{
+    public interface IUserRepository:IRepository<UserModel>
+    {
+        UserModel GetByEmail(string email);
+    }
+}
