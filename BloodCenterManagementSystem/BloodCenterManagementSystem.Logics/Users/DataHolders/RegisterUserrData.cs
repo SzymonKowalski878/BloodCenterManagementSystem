7754958338f6bde2d210d033062ce4c622b38db8
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:49:59 +0100

    Created RegisterUserrData.cs that hold email and password

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Users/DataHolders/RegisterUserrData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Users.DataHolders
+{
+    public class RegisterUserrData
+    {
+        public string Email { get;set; }
+        public string Password { get; set; }
+    }
+}
