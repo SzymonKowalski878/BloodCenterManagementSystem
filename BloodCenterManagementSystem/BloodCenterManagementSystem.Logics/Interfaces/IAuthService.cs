f34310d5bb32d95c1e1c62c4a918238298311c2a
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:18:28 +0100

    Created AuthSerivce with HashPassword method

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Interfaces
+{
+    public interface IAuthService:ILogic
+    {
+        string HashPassword(string password);
+    }
+}
