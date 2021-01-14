369ef96c63843cd1d879518dfe197f66fbbc17f3
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:08:12 +0100

    Created BloodTypeRepository and created GetByBloodTypeName method

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/BloodTypeRepository.cs
@@ -0,0 +1,23 @@
+﻿using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class BloodTypeRepository:Repository<BloodTypeModel>,IBloodTypeRepository
+    {
+        public BloodTypeRepository(Lazy<DataContext> dataContext)
+            : base(dataContext)
+        {
+
+        }
+
+        public BloodTypeModel GetByBloodTypeName(string bloodTypeName)
+        {
+            return DataContext.BloodTypes.FirstOrDefault(m => m.BloodTypeName == bloodTypeName);
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Repositories/IBloodTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Repositories
+{
+    public interface IBloodTypeRepository:IRepository<BloodTypeModel>
+    {
+        BloodTypeModel GetByBloodTypeName(string bloodTypeName);
+    }
+}
