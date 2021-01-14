313e527c0a1807a94f17b18088eb3bc74db6a685
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:14:44 +0100

    Created mappings for data models, set primary keys, indexes and relations

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/Mappings/BloodDonatorMapping.cs
@@ -0,0 +1,32 @@
+﻿using BloodCenterManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess.Mappings
+{
+    public class BloodDonatorMapping : IEntityTypeConfiguration<BloodDonatorModel>
+    {
+        public void Configure(EntityTypeBuilder<BloodDonatorModel> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .HasOne(m => m.User)
+                .WithOne(m => m.BloodDonator)
+                .HasForeignKey<UserModel>(m => m.BloodDonatorId);
+
+            builder
+                .HasOne(m => m.BloodType)
+                .WithMany()
+                .HasForeignKey(m => m.BloodTypeId);
+
+            builder
+                .HasIndex(m => m.Pesel)
+                .IsUnique();
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/Mappings/UserMapping.cs
@@ -0,0 +1,26 @@
+﻿using BloodCenterManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess.Mappings
+{
+    public class UserMapping:IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .HasIndex(m => m.Email)
+                .IsUnique();
+
+            builder
+                .Property(m => m.Email)
+                .HasMaxLength(45);
+        }
+    }
+}
