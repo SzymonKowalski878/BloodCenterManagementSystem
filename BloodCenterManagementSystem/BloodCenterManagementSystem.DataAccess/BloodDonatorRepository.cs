dc102d2b4dc593657a6a0a571027b89bfdbaad8d
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:12:19 +0100

    Created BloodDonatorRepository

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/BloodDonatorRepository.cs
@@ -0,0 +1,17 @@
+﻿using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class BloodDonatorRepository:Repository<BloodDonatorModel>,IBloodDonatorRepository
+    {
+        public BloodDonatorRepository(Lazy<DataContext> dataContext)
+            :base(dataContext)
+        {
+
+        }
+    }
+}
