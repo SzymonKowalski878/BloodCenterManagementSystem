3160a20424aac0ae5daa943926a64d89c9ac7cac
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:04:03 +0100

    Created data models that are needed for blood donator and user account registration

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Models
+{
+    public class UserModel:BaseModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Role { get; set; }
+        public virtual int BloodDonatorId { get; set; }
+        public virtual BloodDonatorModel BloodDonator { get; set; }
+    }
+}
