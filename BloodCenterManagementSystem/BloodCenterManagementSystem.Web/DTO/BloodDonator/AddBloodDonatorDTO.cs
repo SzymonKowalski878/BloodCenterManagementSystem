17474d79f51fb6c6f4d000ec1771a51441a1021f
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-14 02:46:39 +0100

    Created DTOs

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/DTO/BloodDonator/AddBloodDonatorDTO.cs
@@ -0,0 +1,17 @@
+﻿using BloodCenterManagementSystem.Web.DTO.BloodType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.DTO
+{
+    public class AddBloodDonatorDTO
+    {
+        public string Pesel { get; set; }
+        public string HomeAdress { get; set; }
+        public string PhoneNumber { get; set; }
+        public BloodTypeNameDTO BloodType { get; set; }
+        public AddUserDTO User { get; set; }
+    }
+}
