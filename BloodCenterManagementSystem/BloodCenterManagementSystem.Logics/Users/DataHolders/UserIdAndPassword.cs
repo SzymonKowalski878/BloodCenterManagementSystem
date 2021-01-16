d02ae92d64755f142b4ed81bbea39c4c3024b3c2
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-16 16:06:10 +0100

    Added dataholders

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Users/DataHolders/UserIdAndPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Users.DataHolders
+{
+    public class UserIdAndPassword
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Users/DataHolders/UserToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Users.DataHolders
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+    }
+}
