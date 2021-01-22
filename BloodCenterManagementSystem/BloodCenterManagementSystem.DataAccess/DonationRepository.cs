cc841a8dbaa37be4fcc3e61de43f1d36ab2de31f
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-22 17:04:34 +0100

    Created IDonationRepository and DoationRepository

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.DataAccess/DonationRepository.cs
@@ -0,0 +1,19 @@
+﻿using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.DataAccess
+{
+    public class DonationRepository: Repository<DonationModel>, IDonationRepository
+    {
+        public DonationRepository(Lazy<DataContext> dataContext)
+            :base(dataContext)
+        {
+
+        }
+
+
+    }
+}
