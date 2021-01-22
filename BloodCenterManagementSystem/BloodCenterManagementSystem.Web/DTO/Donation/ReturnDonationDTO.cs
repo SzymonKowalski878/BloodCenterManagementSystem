66b5f71423a96006765421b533037ab99b890206
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-22 17:09:05 +0100

    Created ReturnDonationDTO

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/DTO/Donation/ReturnDonationDTO.cs
@@ -0,0 +1,16 @@
+﻿using BloodCenterManagementSystem.Web.DTO.BloodDonator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.DTO.Donation
+{
+    public class ReturnDonationDTO
+    {
+        public int Id { get; set; }
+        public DateTime DonationDate { get; set; }
+        public string RejectionReason { get; set; }
+        public ReturnDonatorInformationDTO BloodDonator { get; set; }
+    }
+}
