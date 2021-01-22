0e1d58321deb467bcb816337278a369b02afbadf
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-22 17:05:05 +0100

    Created IdHolder

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Donations/DataHolders/IdHolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Donations.DataHolders
+{
+    public class IdHolder
+    {
+        public int Id { get; set; }
+    }
+}
