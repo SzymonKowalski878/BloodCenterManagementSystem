bd72dacf36e8896e42a5d984caa035244a6c42bb
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-22 17:09:26 +0100

    Created DonationProfile and Mapping

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/Mappers/DonationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BloodCenterManagementSystem.Models;
+using BloodCenterManagementSystem.Web.DTO.Donation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.Mappers
+{
+    public class DonationProfile:Profile
+    {
+        public DonationProfile()
+        {
+            CreateMap<DonationModel, ReturnDonationDTO>();
+        }
+    }
+}
