889f344bf7c9459ba80edc54b836ddf0c297fa05
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-27 01:02:17 +0100

    Created ResultOfExaminationLogic and IResultOfExaminationLogic created update and add result of examination, created examination controller and Add and update result of examination

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/ResultsOfExaminations/ResultOfExaminationLogic.cs
@@ -0,0 +1,75 @@
+﻿using BloodCenterManagementSystem.Logics.Donations.DataHolders;
+using BloodCenterManagementSystem.Logics.Interfaces;
+using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.ResultsOfExaminations
+{
+    public class ResultOfExaminationLogic:IResultOfExaminationLogic
+    {
+        private readonly Lazy<IResultOfExaminationRepository> _resultOfExaminationRepository;
+        protected IResultOfExaminationRepository ResultOfExaminationRepository => _resultOfExaminationRepository.Value;
+
+        private readonly Lazy<IDonationRepository> _donationRepository;
+        protected IDonationRepository DonationRepository => _donationRepository.Value;
+
+        public ResultOfExaminationLogic(Lazy<IResultOfExaminationRepository> resultOfExaminationRepository,
+            Lazy<IDonationRepository> donationRepository)
+        {
+            _resultOfExaminationRepository = resultOfExaminationRepository;
+            _donationRepository = donationRepository;
+        }
+
+        public Result<ResultOfExaminationModel> AddResults(ResultOfExaminationModel examination)
+        {
+            if (examination == null)
+            {
+                return Result.Error<ResultOfExaminationModel>("IdHolder containing donation id was null");
+            }
+
+            var donation = DonationRepository.GetById(examination.DonationId);
+
+            if(donation == null)
+            {
+                return Result.Error<ResultOfExaminationModel>("Unable to find donation with passed id");
+            }
+
+            if (donation.ResultOfExamination != null)
+            {
+                return Result.Error<ResultOfExaminationModel>("Result already exists");
+            }
+
+            donation.ResultOfExamination = examination;
+            DonationRepository.SaveChanges();
+
+            return Result.Ok(examination);
+        }
+
+        public Result<ResultOfExaminationModel> UpdateResults(ResultOfExaminationModel examination)
+        {
+            if (examination == null)
+            {
+                return Result.Error<ResultOfExaminationModel>("IdHolder containing donation id was null");
+            }
+
+            var resultOfExamination = ResultOfExaminationRepository.GetById(examination.DonationId);
+
+            if (resultOfExamination == null)
+            {
+                return Result.Error<ResultOfExaminationModel>("Unable to find donation with passed id");
+            }
+
+            resultOfExamination.BloodPressureLower = examination.BloodPressureLower;
+            resultOfExamination.BloodPressureUpper = examination.BloodPressureUpper;
+            resultOfExamination.Height = examination.Height;
+            resultOfExamination.Weight = examination.Weight;
+
+            ResultOfExaminationRepository.SaveChanges();
+
+            return Result.Ok(examination);
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/Controllers/ExaminationController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BloodCenterManagementSystem.Logics.Interfaces;
+using BloodCenterManagementSystem.Models;
+using BloodCenterManagementSystem.Web.DTO.ResultOfExamination;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExaminationController:Controller
+    {
+        private readonly Lazy<IResultOfExaminationLogic> _resultOfExamintaionLogic;
+        protected IResultOfExaminationLogic ResultOfExaminationLogic => _resultOfExamintaionLogic.Value;
+
+        private readonly Lazy<IMapper> _mapper;
+        protected IMapper Mapper => _mapper.Value;
+
+        public ExaminationController(Lazy<IResultOfExaminationLogic> resultOfExaminationLogic,
+            Lazy<IMapper> mapper)
+        {
+            _resultOfExamintaionLogic = resultOfExaminationLogic;
+            _mapper = mapper;
+        }
+
+        [HttpPost, Route("AddExamination")]
+        public IActionResult AddExamination(AddResultOfExaminationDTO data)
+        {
+            var dataToAdd = Mapper.Map<AddResultOfExaminationDTO, ResultOfExaminationModel>(data);
+
+            var result = ResultOfExaminationLogic.AddResults(dataToAdd);
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            return Ok(data);
+        }
+
+        [HttpPost,Route("UpdateExamination")]
+        public IActionResult UpdateExamination(UpdateResultOfExaminationDTO data)
+        {
+            var dataToAdd = Mapper.Map<UpdateResultOfExaminationDTO, ResultOfExaminationModel>(data);
+
+            var result = ResultOfExaminationLogic.UpdateResults(dataToAdd);
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            return Ok(data);
+        }
+    }
+}
