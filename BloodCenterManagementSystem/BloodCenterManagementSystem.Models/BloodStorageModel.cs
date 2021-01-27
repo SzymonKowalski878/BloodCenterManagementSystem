f5fc0e13125b01d00e7aaf94c3bcb15311717230
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-27 20:50:18 +0100

    changed int to int? in BloodStorageModel

--- BloodCenterManagementSystem/BloodCenterManagementSystem.Models/BloodStorageModel.cs
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Models/BloodStorageModel.cs
@@ -6,13 +6,13 @@ namespace BloodCenterManagementSystem.Models
 {
     public class BloodStorageModel:BaseModel
     {
-        public int ForeignBloodUnitId { get; set; }
+        public int? ForeignBloodUnitId { get; set; }
         public string BloodUnitLocation { get; set; }
         public bool IsAvailable { get; set; }
         public bool IsAfterCovid { get; set; }
         public int BloodTypeId { get; set; }
         public BloodTypeModel BloodType { get; set; }
-        public int DonationId { get; set; }
+        public int? DonationId { get; set; }
         public DonationModel Donation { get; set; }
     }
 }
