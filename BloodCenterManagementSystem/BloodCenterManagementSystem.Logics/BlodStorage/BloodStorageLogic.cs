fb1b42229534850dab5ac358b16bcd114842af57
SzymonKowalski <szymonkowalski878@gmail.com>
2021-01-27 20:53:16 +0100

    Created BloodStorage Logic and BloodStorageController, created AddBloodUnit,AddForeignBloodUnit,ReturnAllAvailableBloodUnits,ChangeBloodUnitToUnavailable methods and actions

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/BlodStorage/BloodStorageLogic.cs
@@ -0,0 +1,142 @@
+﻿using BloodCenterManagementSystem.Logics.BlodStorage.DataHolders;
+using BloodCenterManagementSystem.Logics.Donations.DataHolders;
+using BloodCenterManagementSystem.Logics.Interfaces;
+using BloodCenterManagementSystem.Logics.Repositories;
+using BloodCenterManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.BlodStorage
+{
+    public class BloodStorageLogic:IBloodStorageLogic
+    {
+        private readonly Lazy<IBloodStorageRepository> _bloodStorageRepository;
+        protected IBloodStorageRepository BloodStorageRepository => _bloodStorageRepository.Value;
+
+        private readonly Lazy<IDonationRepository> _donationRepository;
+        protected IDonationRepository DonationRepository => _donationRepository.Value;
+
+        private readonly Lazy<IBloodTypeRepository> _bloodTypeRepository;
+        protected IBloodTypeRepository BloodTypeRepository => _bloodTypeRepository.Value;
+
+        public BloodStorageLogic(Lazy<IBloodStorageRepository> bloodStorageRepository,
+            Lazy<IDonationRepository> donationRepository,
+            Lazy<IBloodTypeRepository> bloodTypeRepository)
+        {
+            _bloodStorageRepository = bloodStorageRepository;
+            _donationRepository = donationRepository;
+            _bloodTypeRepository = bloodTypeRepository;
+        }
+
+        public Result<BloodStorageModel> AddBloodUnit(AddBloodUnitToStorage data)
+        {
+            if (data == null)
+            {
+                return Result.Error<BloodStorageModel>("Data containing donation id was null");
+            }
+
+            var donation = DonationRepository.GetById(data.DonationId);
+
+            if(donation==null || donation.BloodDonator == null)
+            {
+                return Result.Error<BloodStorageModel>("Unable to find donation with such id or blood donator was null");
+            }
+
+            if (donation.BloodStorage != null)
+            {
+                return Result.Error<BloodStorageModel>("Blood unit already in database");
+            }
+
+            if (donation.Stage != "qualified")
+            {
+                return Result.Error<BloodStorageModel>("Wrong donation stage");
+            }
+
+            var unit = new BloodStorageModel
+            {
+                Id = 0,
+                ForeignBloodUnitId = null,
+                BloodUnitLocation=null,
+                IsAvailable=true,
+                IsAfterCovid=data.IsAfterCovid,
+                BloodTypeId=donation.BloodDonator.BloodTypeId,
+                DonationId = donation.Id,
+            };
+
+            BloodStorageRepository.Add(unit);
+            BloodStorageRepository.SaveChanges();
+
+            return Result.Ok(unit);
+        }
+
+        public Result<BloodStorageModel> AddForeignBloodUnit(AddForeignBloodUnitToStorage data)
+        {
+            if (data == null)
+            {
+                return Result.Error<BloodStorageModel>("Data passed in the function was null");
+            }
+
+            if (string.IsNullOrEmpty(data.BloodUnitLocation))
+            {
+                return Result.Error<BloodStorageModel>("Blood unit location was null or empty");
+            }
+
+            var bloodType = BloodTypeRepository.GetByBloodTypeName(data.BloodTypeName);
+
+            if (bloodType == null)
+            {
+                return Result.Error<BloodStorageModel>("Unable to find blood type");
+            }
+
+            var unit = new BloodStorageModel
+            {
+                Id = 0,
+                ForeignBloodUnitId = data.ForeignBloodUnitId,
+                BloodUnitLocation = data.BloodUnitLocation,
+                IsAvailable = true,
+                IsAfterCovid = data.IsAfterCovid,
+                DonationId = null,
+                BloodType = bloodType
+            };
+
+            BloodStorageRepository.Add(unit);
+            BloodStorageRepository.SaveChanges();
+
+            return Result.Ok(unit);
+        }
+
+        public Result<IEnumerable<BloodStorageModel>> ReturnAllAvailableBloodUnits()
+        {
+            var units = BloodStorageRepository.GetAll();
+
+            if (units.Count() == 0)
+            {
+                return Result.Error<IEnumerable<BloodStorageModel>>("No available blood units");
+            }
+
+            return Result.Ok(units);
+        }
+
+        public Result<BloodStorageModel> ChangeBloodUnitToUnavailable(IdHolder bloodUnitId)
+        {
+            if(bloodUnitId== null)
+            {
+                return Result.Error<BloodStorageModel>("bloo unit id data was null");
+            }
+
+            var unit = BloodStorageRepository.GetById(bloodUnitId.Id);
+
+            if(unit == null)
+            {
+                return Result.Error<BloodStorageModel>("Unable to find blood unit");
+            }
+
+            unit.IsAvailable = false;
+            BloodStorageRepository.SaveChanges();
+
+            return Result.Ok(unit);
+        }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Web/Controllers/BloodStorageController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using BloodCenterManagementSystem.Logics.BlodStorage.DataHolders;
+using BloodCenterManagementSystem.Logics.Donations.DataHolders;
+using BloodCenterManagementSystem.Logics.Interfaces;
+using BloodCenterManagementSystem.Models;
+using BloodCenterManagementSystem.Web.DTO.BloodStorage;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodCenterManagementSystem.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BloodStorageController:Controller
+    {
+        private readonly Lazy<IBloodStorageLogic> _bloodStorageLogic;
+        protected IBloodStorageLogic BloodStorageLogic => _bloodStorageLogic.Value;
+
+        private readonly Lazy<IMapper> _mapper;
+        protected IMapper Mapper => _mapper.Value;
+
+        public BloodStorageController(Lazy<IBloodStorageLogic> bloodStorageLogic,
+            Lazy<IMapper> mapper)
+        {
+            _bloodStorageLogic = bloodStorageLogic;
+            _mapper = mapper;
+        }
+
+        [HttpPost,Route("AddBloodUnitToStorage")]
+        public IActionResult AddBloodUnitToStorage(AddBloodUnitToStorage data)
+        {
+            var result = BloodStorageLogic.AddBloodUnit(data);
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            var toReturn = Mapper.Map<BloodStorageModel, ReturnAddedUnitDTO>(result.Value);
+
+            return Ok(toReturn);
+        }
+
+        [HttpPost,Route("AddForeignBloodUnitToStorage")]
+        public IActionResult AddForeingBloodUnitToStorage(AddForeignBloodUnitToStorage data)
+        {
+            var result = BloodStorageLogic.AddForeignBloodUnit(data);
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            var toReturn = Mapper.Map<BloodStorageModel, ReturnAddedUnitDTO>(result.Value);
+
+            return Ok(toReturn);
+        }
+
+        [HttpGet,Route("ReturnAllAvailableBloodUnits")]
+        public IActionResult ReturnAllAvailableBloodUnits()
+        {
+            var result = BloodStorageLogic.ReturnAllAvailableBloodUnits();
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            var list = new List<ReturnAddedUnitDTO>();
+
+            foreach(var x in result.Value)
+            {
+                list.Add(Mapper.Map<BloodStorageModel, ReturnAddedUnitDTO>(x));
+            }
+
+            return Ok(list);
+        }
+
+        [HttpPost,Route("ChangeBloodUnitToUnavailable")]
+        public IActionResult ChangeBloodUnitToUnavailable(IdHolder bloodUnitId)
+        {
+            var result = BloodStorageLogic.ChangeBloodUnitToUnavailable(bloodUnitId);
+
+            if (!result.IsSuccessfull)
+            {
+                return BadRequest(result.ErrorMessages);
+            }
+
+            return Ok(result);
+        }
+    }
+}
