a01b47e68935e6f1b2f2562b842476893e3cafcd
SzymonKowalski <szymonkowalski878@gmail.com>
2021-08-29 23:38:26 +0200

    New data model, and data holders

---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Logics/Users/DataHolders/PasswordHolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Logics.Users.DataHolders
+{
+    public class PasswordHolder
+    {
+        public string Password { get; set; }
+    }
+}
---
+++ BloodCenterManagementSystem/BloodCenterManagementSystem.Models/EmailConfigModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodCenterManagementSystem.Models
+{
+    public class EmailConfigModel
+    {
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+    }
+}
